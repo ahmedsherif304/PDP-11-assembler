e42614d46c92329503d9a259efdbd7379d833aba
ahmedsherif304 <ahmedsamora24@yahoo.com>
2020-12-21 17:09:20 +0200

    removing comments and separating vars from code

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 file = open("test.txt")
+string = ""
+for line in file:
+    # split the line and get what is before the ; symbol and then make all the charchters  upper cases
+    line = line.split(';')[0].upper()
+    if line != "\n":
+        string = string + line
+
+code = string.split("HLT")[0]
+variables = string.split("HLT")[1].split("DEFINE")
+print(variables)
+print(code)
